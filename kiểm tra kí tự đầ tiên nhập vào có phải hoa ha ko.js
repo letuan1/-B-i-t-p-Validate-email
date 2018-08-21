35c77f96a0a96fd0f54b097a8cce271204935832
letuan1 <41882914+letuan1@users.noreply.github.com>
2018-08-21 15:14:23 +0700

    Update kiểm tra kí tự đầ tiên nhập vào có phải hoa ha ko.js

--- kiểm tra kí tự đầ tiên nhập vào có phải hoa ha ko.js
+++ kiểm tra kí tự đầ tiên nhập vào có phải hoa ha ko.js
@@ -1,10 +1,9 @@
-function upperCase(str){
-    str = prompt("nhập 1 chuỗi kí tự");
-    regexp = /^[A-Z]/;
-    if (regexp.test(str)) {
-        alert("String's first character is uppercase");
+function email(src) {
+    src = prompt("nhập 1 email để kiểm tra xem có hợp lệ hay không");
+    regexp = /^[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z0-9]+$/;
+    if (regexp.test(src)) {
+        alert("email hợp lệ")
     } else {
-        alert("String's first character is not uppercase");
+        alert("email không hợp lệ")
     }
 }
-
